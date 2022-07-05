6c5f4fcfec3b997c5ef5899436baf2d5e1ebf2ea
Daniel <danielmarchessi2014@gmail.com>
2022-07-05 07:39:57 -0500

    Creacion Proyecto

---
+++ src/main/java/com/alkemy/icons/ApiAlkemy1Application.java
@@ -0,0 +1,13 @@
+package com.alkemy.icons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlkemy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAlkemy1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/icons/ApiAlkemy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.icons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAlkemy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
